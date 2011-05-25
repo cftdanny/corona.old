8c81b603f728cf2d2758e855c39000980cf4e6d3
Danny Chen <cftdanny@hotmail.com>
2011-05-25 22:26:20 +0800

    OPEN - task 27: Start transaction before call producer method http://github.com/cftdanny/corona/issues/issue/27

--- src/com/corona/servlet/ComponentHandler.java
+++ src/com/corona/servlet/ComponentHandler.java
@@ -17,10 +17,16 @@ import javax.servlet.http.HttpSession;
 import com.corona.context.ContextManager;
 import com.corona.context.ContextManagerFactory;
 import com.corona.context.Key;
+import com.corona.context.extension.DecoratedMethod;
+import com.corona.data.ConnectionManager;
+import com.corona.data.ConnectionManagerFactory;
+import com.corona.data.Transaction;
+import com.corona.data.TransactionManager;
 import com.corona.logging.Log;
 import com.corona.logging.LogFactory;
 import com.corona.servlet.annotation.ContentType;
 import com.corona.servlet.annotation.Expiration;
+import com.corona.servlet.annotation.Transactional;
 
 /**
  * <p>This handler is used to create HTTP response by an injection method in component. </p>
@@ -127,7 +133,14 @@ class ComponentHandler extends AbstractHandler {
 		Object component = contextManager.get(this.producer.getKey());
 		
 		// invoke annotated method and produce web content by template and method result 
-		Object outcome = this.producer.getDecoratedMethod().invoke(contextManager, component);
+		Object outcome = null;
+		if (this.producer.getDecoratedMethod().getMethod().isAnnotationPresent(Transactional.class)) {
+			outcome = this.execute(contextManager, component, this.producer.getDecoratedMethod());
+		} else {
+			outcome = this.producer.getDecoratedMethod().invoke(contextManager, component);
+		}
+		
+		// create response output by producer's outcome
 		try {
 			this.producer.produce(contextManager, response, response.getOutputStream(), outcome);
 		} catch (Throwable e) {
@@ -144,5 +157,97 @@ class ComponentHandler extends AbstractHandler {
 			this.logger.error("Fail to flush produced content to HTTP response", e);
 			throw new HandleException("Fail to flush produced content to HTTP response", e);
 		}
+	}
+	
+	/**
+	 * @param contextManager current context manager
+	 * @param component the component
+	 * @param method the producer method
+	 * @return the outcome return by producer method
+	 * @throws HandleException if fail to invoke method in transaction
+	 */
+	private Object execute(
+			final ContextManager contextManager, final Object component, final DecoratedMethod method
+	) throws HandleException {
+
+		// open default connection manager from context container
+		ConnectionManager connectionManager = null;
+		try {
+			connectionManager = this.getConnectionManager(contextManager);
+		} catch (Exception e) {
+			throw new HandleException("Fail to get default connection manager, maybe is not registered", e);
+		}
+
+		// try to get container managed transaction first, if can'n, get connection manager's transaction
+		Transaction transaction = this.getTransaction(contextManager);
+		if (transaction == null) {
+			transaction = connectionManager.getTransaction();
+		}
+
+		// try to start transaction, if can't, throw exception and close connection manager
+		try {
+			transaction.begin();
+		} catch (Exception e) {
+			
+			// fail start transaction, will close connection manager
+			try {
+				connectionManager.close();
+			} catch (Exception e1) {
+				throw new HandleException("Fail to close default connection manager", e);
+			}
+			throw new HandleException("Fail to start transaction before invoke producer method", e);
+		}
+		
+		// execute producer business logic method in transaction
+		try {
+			Object outcome = method.invoke(contextManager, component);
+			if (!transaction.getRollbackOnly()) {
+				transaction.commit();
+			} else {
+				transaction.rollback();
+			}
+			return outcome;
+			
+		} catch (Throwable e) {
+			
+			try {
+				transaction.rollback();
+			} catch (Exception ex) {
+				this.logger.warn("Fail to roll back , but do not know how to handle this case, just ignore", e);
+			}
+			
+			this.logger.error("Fail to invoke method [{0}] within transaction, will roll back", e, method);
+			throw new HandleException("Fail to invoke method [{0}] within transaction, will roll back", e, method);
+			
+		} finally {
+			
+			// try to close connection manager, if error, just skip it
+			try {
+				connectionManager.close();
+			} catch (Exception e) {
+				this.logger.warn(
+						"Fail to close connection manager, but changed data has been commit or roll back", e
+				);
+			}
+		}
+	}
+	
+	/**
+	 * @param contextManager the current context manager
+	 * @return the default connection manager
+	 * @throws Exception if fail to open connection manager
+	 */
+	private ConnectionManager getConnectionManager(final ContextManager contextManager) throws Exception {
+		return contextManager.get(ConnectionManagerFactory.class).open();
+	}
+	
+	/**
+	 * @param contextManager the current context manager
+	 * @return the container managed transaction
+	 */
+	private Transaction getTransaction(final ContextManager contextManager) {
+
+		TransactionManager transactionManager = contextManager.get(TransactionManager.class);
+		return (transactionManager != null) ? transactionManager.getTransaction() : null;
 	}
 }
