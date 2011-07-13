32f010912c0fb2e0f0c9c554cf50153538415cc1
Danny Chen <cftdanny@hotmail.com>
2011-07-13 23:36:10 +0800

    OPEN - task 90: User visiting statistic http://github.com/cftdanny/corona/issues/issue/90

---
+++ src/com/corona/servlet/tracking/Tracks.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2009 Aurora Software Technology Studio. All rights reserved.
+ */
+package com.corona.servlet.tracking;
+
+
+/**
+ * <p> </p>
+ *
+ * @author $Author$
+ * @version $Id$
+ */
+public class Tracks {
+
+}
---
+++ src/com/corona/servlet/tracking/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2009 Aurora Software Technology Studio. All rights reserved.
+ */
+package com.corona.servlet.tracking;
+
+/**
+ * track what resource is accessed by user 
+ */
+ 
